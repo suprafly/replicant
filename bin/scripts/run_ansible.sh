ce9f9c84244405d7cb7dc3e7fac61b989c02b25e
Chris <chris.a.lyman@gmail.com>
2016-10-28 22:02:31 -0700

    fix run_ansible.sh bug

--- bin/scripts/run_ansible.sh
+++ bin/scripts/run_ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$USER sudo ansible-playbook -i "localhost," -c local bin/ansible/playbook.yml --extra-vars "username=$USER cwd=$PWD"
+sudo ansible-playbook -i "localhost," -c local bin/ansible/playbook.yml --extra-vars "username=$USER cwd=$PWD"
